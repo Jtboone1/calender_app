a16b7830a4efa645034c863c3cf2b78666441962
Jarrod Boone <boojarrod@gmail.com>
2021-03-24 00:38:07 -0230

    Added functionality to todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { v4 as uuidv4 } from 'uuid';
+import TodoItem from "./TodoItem";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,24 +28,67 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimplePaper() {
   const classes = useStyles();
-  const [todoList, setTodoList] = useState([]);
-  const [newTodo, setNewTodo] = useState("")
+  const [todoList, setTodoList] = useState([
+      {
+          id: uuidv4(),
+          task: "Take out trash",
+          finished: true
+      },
+      {
+        id: uuidv4(),
+        task: "Finish Homework",
+        finished: false
+    },
+  ]);
+  const [todoName, setTodoName] = useState("")
 
-  const doit = (newText) => {
+  const addTodo = (task) => {
 
+    if (task !== "") {
+        const newTodo = {
+            id: uuidv4(),
+            task,
+            finished: false
+        };
+    
+        setTodoList([...todoList, newTodo]);
+        setTodoName("");
+    }
   }
+
+  const deleteTodo = id => {
+      let newTodoList = todoList.filter(todo => todo.id !== id);
+      setTodoList(newTodoList);
+  };
+
+   // Toggle Finished
+   const markFinished = id => {
+    console.log(id)
+    setTodoList(todoList.map(todo => {
+        if (todo.id === id) {
+          todo.finished = !todo.finished;
+        }
+        return todo;
+    }))
+  };
+
 
   return (
     <div className={classes.root}>
 
       <Paper elevation={3} >
         <form className={classes.text} noValidate autoComplete="off">
-          <TextField id="standard-basic" label="New Task" style={addTodoStyle}/>
-          <Button variant="contained" color="primary" style={addButtonStyle} onClick={() => doit(newTodo)}>Add</Button>
-          <ul>
-            
-          </ul>
+          <TextField id="standard-basic"  value={todoName} label="New Task" style={addTodoStyle} onChange={(e) => setTodoName(e.target.value)}/>
+          <Button variant="contained" color="primary" style={addButtonStyle} onClick={() => addTodo(todoName)}>Add</Button>
+          
         </form>
+        {todoList.map((todo) => (
+          <TodoItem 
+            key={todo.id} 
+            todo={todo}  
+            deleteTodo={deleteTodo}
+            markFinished={markFinished}/>
+        ))}
       </Paper>
 
     </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
 
-export default function TodoItem() {
+export default function TodoItem(props) {
+    const lineThroughStyle = () => {
+        return {
+
+          textDecoration: props.todo.finished ? "line-through" : "none",
+        };
+      };
+
+
     return (
-        <div>
-            
+        <div style={{marginTop: "20px"}}>
+          <div style={lineThroughStyle()}>{props.todo.task}
+            <Button variant="contained" color="secondary" onClick={() => props.deleteTodo(props.todo.id)} style={{float: "right", marginRight: "10px"}}>
+              Remove
+            </Button>
+            <div >
+              <button onClick={() => props.markFinished(props.todo.id)}>Complete</button>
+            </div>
+          </div>
+          
         </div>
+        
     )
 }
